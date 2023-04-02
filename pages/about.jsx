3e97ba08a1dbafd5bb78fb0cadc027850652cc73
LukaBendeliani <luka.bendela@gmail.com>
2023-04-02 20:36:13 +0400

    removed unused code

--- pages/about.jsx
+++ pages/about.jsx
@@ -73,12 +73,137 @@ const About = () => {
             </div>
           </div>
         </div>
+        {/* Section Contacts Form */}
+        <div className="section section-inner m-contacts-form">
+          <div className="container">
+            <div className="row">
+              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+                {/* titles */}
+                <div className="m-titles">
+                  <div
+                    className="m-title scrolla-element-anim-1 scroll-animate"
+                    data-animate="active"
+                  >
+                    Get in touch
+                  </div>
+                </div>
+                {/* contact form */}
+                <div className="contacts-form">
+                  <form id="cform" method="post">
+                    <div className="group">
+                      <div
+                        className="value scrolla-element-anim-1 scroll-animate"
+                        data-animate="active"
+                      >
+                        <input
+                          type="text"
+                          name="name"
+                          placeholder="Full Name"
+                        />
+                      </div>
+                    </div>
+                    <div className="group">
+                      <div
+                        className="value scrolla-element-anim-1 scroll-animate"
+                        data-animate="active"
+                      >
+                        <input
+                          type="text"
+                          name="email"
+                          placeholder="Email Address"
+                        />
+                      </div>
+                    </div>
+                    <div className="group full">
+                      <div
+                        className="value scrolla-element-anim-1 scroll-animate"
+                        data-animate="active"
+                      >
+                        <textarea
+                          name="message"
+                          placeholder="Message"
+                          defaultValue={""}
+                        />
+                      </div>
+                    </div>
+                    <div
+                      className="submit scrolla-element-anim-1 scroll-animate"
+                      data-animate="active"
+                    >
+                      <a href="#" className="btn">
+                        Send a Message
+                      </a>
+                    </div>
+                  </form>
+                  <div className="alert-success" style={{ display: "none" }}>
+                    <p>Thanks, your message is sent successfully.</p>
+                  </div>
+                </div>
+              </div>
+              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-6">
+                {/* titles */}
+                <div className="m-titles">
+                  <h2
+                    className="m-title scrolla-element-anim-1 scroll-animate"
+                    data-animate="active"
+                  >
+                    Contact info
+                  </h2>
+                </div>
+                {/* services */}
+                <div className="services-items row">
+                  <div className="services-col col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                    <div
+                      className="services-item scrolla-element-anim-1 scroll-animate"
+                      data-animate="active"
+                    >
+                      <div className="icon">
+                        <i aria-hidden="true" className="fas fa-phone-alt" />
+                      </div>
+                      <div className="name">Phone:</div>
+                      <div className="text">+1 (800) 123 56 89</div>
+                    </div>
+                  </div>
+                  <div className="services-col col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                    <div
+                      className="services-item scrolla-element-anim-1 scroll-animate"
+                      data-animate="active"
+                    >
+                      <div className="icon">
+                        <i aria-hidden="true" className="fas fa-at" />
+                      </div>
+                      <div className="name">E-mail:</div>
+                      <div className="text">aster@domain.com</div>
+                    </div>
+                  </div>
+                  <div className="services-col col-xs-12 col-sm-6 col-md-6 col-lg-4">
+                    <div
+                      className="services-item scrolla-element-anim-1 scroll-animate"
+                      data-animate="active"
+                    >
+                      <div className="icon">
+                        <i
+                          aria-hidden="true"
+                          className="fab fa-font-awesome-flag"
+                        />
+                      </div>
+                      <div className="name">Location:</div>
+                      <div className="text">
+                        Marollem St. 32, New York, USA.
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         {/* Section Services */}
         <div className="section section-inner m-services">
           <div className="container">
             <div className="row">
-              <div className="col-xs-12 col-sm-12 col-md-12 align-left col-lg-12">
-                {/* titles */}
+              {/* <div className="col-xs-12 col-sm-12 col-md-12 align-left col-lg-12">
+                titles
                 <div className="m-titles">
                   <h2
                     className="m-title scrolla-element-anim-1 scroll-animate"
@@ -87,9 +212,9 @@ const About = () => {
                     Our services
                   </h2>
                 </div>
-              </div>
-              <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                {/* services */}
+              </div> */}
+              {/* <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12"> */}
+              {/* services
                 <div className="services-items row">
                   <div className="services-col col-xs-12 col-sm-6 col-md-6 col-lg-4">
                     <div
@@ -181,12 +306,12 @@ const About = () => {
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
+                </div> */}
+              {/* </div> */}
             </div>
           </div>
         </div>
-        {/* Section Description */}
+        {/* Section Description
         <div className="section section-inner m-description">
           <div className="container">
             <div className="row">
@@ -250,15 +375,15 @@ const About = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         {/* Section Video Large */}
-        <AboutVideo />
+        {/* <AboutVideo /> */}
         {/* Section Team */}
-        <div className="section section-inner m-team">
+        {/* <div className="section section-inner m-team">
           <div className="container">
             <div className="row">
               <div className="col-xs-12 col-sm-12 col-md-12 align-left col-lg-12">
-                {/* titles */}
+                titles
                 <div className="m-titles">
                   <h2
                     className="m-title scrolla-element-anim-1 scroll-animate"
@@ -269,7 +394,7 @@ const About = () => {
                 </div>
               </div>
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                {/* team */}
+                 team 
                 <div className="team-items row">
                   <div className="team-col col-xs-12 col-sm-12 col-md-6 col-lg-4">
                     <div
@@ -323,13 +448,13 @@ const About = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         {/* Section Testimonials */}
-        <div className="section section-inner m-testimonials">
+        {/* <div className="section section-inner m-testimonials">
           <div className="container">
             <div className="row">
               <div className="col-xs-12 col-sm-12 col-md-12 align-left col-lg-12">
-                {/* titles */}
+                 titles 
                 <div className="m-titles">
                   <h2
                     className="m-title scrolla-element-anim-1 scroll-animate"
@@ -340,12 +465,12 @@ const About = () => {
                 </div>
               </div>
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                {/* Carousel */}
+                Carousel 
                 <Swiper
                   {...jsTestimonials}
                   className="swiper-container js-testimonials"
                 >
-                  {/* Testimonials item */}
+                   Testimonials item 
                   <SwiperSlide className="testimonials-item swiper-slide">
                     <div
                       className="scrolla-element-anim-1 scroll-animate"
@@ -366,7 +491,7 @@ const About = () => {
                       </div>
                     </div>
                   </SwiperSlide>
-                  {/* Testimonials item */}
+                   Testimonials item 
                   <SwiperSlide className="testimonials-item swiper-slide">
                     <div
                       className="scrolla-element-anim-1 scroll-animate"
@@ -387,7 +512,7 @@ const About = () => {
                       </div>
                     </div>
                   </SwiperSlide>
-                  {/* Testimonials item */}
+                   Testimonials item 
                   <SwiperSlide className="testimonials-item swiper-slide">
                     <div
                       className="scrolla-element-anim-1 scroll-animate"
@@ -408,7 +533,7 @@ const About = () => {
                       </div>
                     </div>
                   </SwiperSlide>
-                  {/* Testimonials item */}
+                   Testimonials item 
                   <SwiperSlide className="testimonials-item swiper-slide">
                     <div
                       className="scrolla-element-anim-1 scroll-animate"
@@ -429,20 +554,20 @@ const About = () => {
                       </div>
                     </div>
                   </SwiperSlide>
-                  {/* Pagination  */}
+                   Pagination  
                   <div className="swiper-pagination" />
                 </Swiper>
-                {/* /Carousel */}
+                 /Carousel 
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
         {/* Section Partners */}
-        <div className="section section-inner m-partners">
+        {/* <div className="section section-inner m-partners">
           <div className="container">
             <div className="row">
               <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-                {/* partners */}
+                 partners 
                 <div className="partners-box">
                   <div className="partners-items row">
                     <div
@@ -510,7 +635,7 @@ const About = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
       {/* Footer */}
       <Footer />
--- src/layout/Header.js
+++ src/layout/Header.js
@@ -3,8 +3,6 @@ import { useEffect, useState } from "react";
 
 const Header = () => {
   const [toggle, setToggle] = useState(false);
-  const [linkElements, setLinkElements] = useState(null);
-  const [height, setHeight] = useState(null);
 
   const toggleFun = (e) => {
     const menu = document.querySelector(".menu-btn.full"),
@@ -40,17 +38,7 @@ const Header = () => {
   useEffect(() => {
     document.querySelector("body").classList.remove("no-scroll");
   }, []);
-  const subMenuClick = (event, value) => {
-      setLinkElements(value == linkElements ? null : value);
-      const target = event.target,
-        ul = target.closest(".menu-full");
-      setHeight(ul.offsetHeight + 20);
-    },
-    subMenuHeight = (value) =>
-      value == linkElements
-        ? { maxHeight: `${height}px` }
-        : { maxHeight: "0px" },
-    subMenuOpened = (value) => (value == linkElements ? "opened" : "closed");
+
   return (
     <header className="header">
       <div className="header__builder">
@@ -75,183 +63,33 @@ const Header = () => {
                   {/* menu full */}
                   <div className="menu-full">
                     <ul className="menu-full">
-                      <li
-                        className={`menu-item menu-item-has-children has-children ${subMenuOpened(
-                          "home"
-                        )}`}
-                      >
-                        <a
-                          className="splitting-text-anim-2 words chars splitting"
-                          data-splitting="chars"
-                          href="#"
-                          onClick={(e) => subMenuClick(e, "home")}
-                        >
-                          Home
-                        </a>
-                        <ul className="sub-menu" style={subMenuHeight("home")}>
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                Parallax Slider
-                              </a>
-                            </Link>
-                          </li>
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/home-grid-slider">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                Grid Slider
-                              </a>
-                            </Link>
-                          </li>
-
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/home-grid-fully-slider">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                Blur Horizontal Slider
-                              </a>
-                            </Link>
-                          </li>
-
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/home-portfolio">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                Portfolio Grid
-                              </a>
-                            </Link>
-                          </li>
-                        </ul>
-                      </li>
-                      <li
-                        className={`menu-item menu-item-has-children has-children ${subMenuOpened(
-                          "about"
-                        )}`}
-                      >
-                        <a
-                          className="splitting-text-anim-2"
-                          data-splitting="chars"
-                          href="#"
-                          onClick={(e) => subMenuClick(e, "about")}
-                        >
-                          About
-                        </a>
-                        <ul className="sub-menu" style={subMenuHeight("about")}>
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/about">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                About One
-                              </a>
-                            </Link>
-                          </li>
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/about-two">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                About Two
-                              </a>
-                            </Link>
-                          </li>
-                        </ul>
-                      </li>
-                      <li
-                        className={`menu-item menu-item-has-children has-children ${subMenuOpened(
-                          "Works"
-                        )}`}
-                      >
-                        <a
-                          className="splitting-text-anim-2 words chars splitting"
-                          data-splitting="chars"
-                          href="#"
-                          onClick={(e) => subMenuClick(e, "Works")}
-                        >
-                          Works
-                        </a>
-                        <ul className="sub-menu" style={subMenuHeight("Works")}>
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/works-2-column">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                Works (2 Columns)
-                              </a>
-                            </Link>
-                          </li>
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/works-3-column">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                Works (3 Columns)
-                              </a>
-                            </Link>
-                          </li>
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/works-creative">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                Works (Creative)
-                              </a>
-                            </Link>
-                          </li>
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/gallery-2-column">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                Gallery (2 Columns)
-                              </a>
-                            </Link>
-                          </li>
-                          <li className="menu-item">
-                            <Link legacyBehavior href="/gallery-3-column">
-                              <a
-                                className="splitting-text-anim-1"
-                                data-splitting="chars"
-                              >
-                                Gallery (3 Columns)
-                              </a>
-                            </Link>
-                          </li>
-                        </ul>
-                      </li>
-                      <li className="menu-item">
-                        <Link legacyBehavior href="/contacts">
+                      <li className={"menu-item"}>
+                        <Link legacyBehavior href="/">
                           <a
-                            className="splitting-text-anim-2"
+                            className="splitting-text-anim-2 words chars splitting"
                             data-splitting="chars"
                           >
-                            Contact
+                            Home
                           </a>
                         </Link>
                       </li>
                       <li className="menu-item">
-                        <Link legacyBehavior href="/blog">
+                        <Link legacyBehavior href="/about">
                           <a
                             className="splitting-text-anim-2"
                             data-splitting="chars"
                           >
-                            Blog
+                            About
+                          </a>
+                        </Link>
+                      </li>
+                      <li className="menu-item">
+                        <Link legacyBehavior href="/works-creative">
+                          <a
+                            className="splitting-text-anim-2 words chars splitting"
+                            data-splitting="chars"
+                          >
+                            Works
                           </a>
                         </Link>
                       </li>
@@ -260,41 +98,6 @@ const Header = () => {
                 </div>
               </div>
             </div>
-          </div>
-          {/* social */}
-          <div className="menu-social-links">
-            <a
-              href="https://instagram.com/"
-              target="blank"
-              className="scrolla-element-anim-1"
-              title="Instagram"
-            >
-              Instagram
-            </a>
-            <a
-              href="https://facebook.com/"
-              target="blank"
-              className="scrolla-element-anim-1"
-              title="Facebook"
-            >
-              Facebook
-            </a>
-            <a
-              href="https://twitter.com/"
-              target="blank"
-              className="scrolla-element-anim-1"
-              title="Twitter"
-            >
-              Twitter
-            </a>
-            <a
-              href="https://linkedin.com/"
-              target="blank"
-              className="scrolla-element-anim-1"
-              title="Linkedin"
-            >
-              Linkedin
-            </a>
           </div>
         </div>
       </div>
